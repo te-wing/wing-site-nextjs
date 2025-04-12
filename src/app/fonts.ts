006e3f640f0b5d211885a7a7704f5413bbe0f96e
Tsubasa <wing@me.wing.osaka>
2025-04-12 23:07:36 +0900

    いろいろ
    fonts.tsは，作っただけ．

---
+++ src/app/fonts.ts
@@ -0,0 +1,10 @@
+import { Playwrite_DK_Loopet, Zen_Maru_Gothic } from 'next/font/google'
+ 
+export const playwrite = Playwrite_DK_Loopet({
+    display: 'swap',
+})
+ 
+export const zenmaru = Zen_Maru_Gothic({
+    display: 'swap',
+    weight: '400'
+})
