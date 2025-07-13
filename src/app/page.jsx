0ede180a09d37edd82475a1ea370d37bebfd84ab
Tsubasa <wing@me.wing.osaka>
2025-07-13 21:41:40 +0900

    ブログも

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,13 +11,24 @@ export default function HomePage() {
     <>
       <h2>Home</h2>
       <section>
-        <div className={styles.homeWidget}>
+        <div className={styles.photoWidget}>
           <div className={styles.widgetContent}>
             <div className={styles.spacer}></div>
             <h3 className={styles.widgetTitle}>Wingが撮影した写真</h3>
             <p>Wingは，写真を撮ることが趣味．とはいっても，自分が写真に映ることはあんまり好きやないね．</p>
             <div className={styles.spacer}></div>
             <Link href='/hobby/photo'>See more..:.</Link>
+            <div className={styles.spacer}></div>
+          </div>
+        </div>
+        <div className={styles.blogWidget}>
+          <div className={styles.widgetContent}>
+            <div className={styles.spacer}></div>
+            <h3 className={styles.widgetTitle}>Wingのブログ！</h3>
+            <p>Wingがブログを開始！とりあえずジャンル関係なく色々書いてます．まだまだ模索中っていう感じなんで，ご意見とかあったらぜひ，連絡くださいね．</p>
+            <div className={styles.spacer}></div>
+            <a href='https://r.wing.osaka/s/blog' target='_blank'>See more..:.</a>
+            <div className={styles.spacer}></div>
           </div>
         </div>
       </section>
