c62ee90fcb3f8290efae1cf00f726cd5cb4630b8
Tsubasa <wing@me.wing.osaka>
2025-07-13 19:52:31 +0900

    写真

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import styles from './page.module.scss';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Wingのサイト',
@@ -9,6 +10,17 @@ export default function HomePage() {
   return (
     <>
       <h2>Home</h2>
+      <section>
+        <div className={styles.homeWidget}>
+          <div className={styles.widgetContent}>
+            <div className={styles.spacer}></div>
+            <h3 className={styles.widgetTitle}>Wingが撮影した写真</h3>
+            <p>Wingは，写真を撮ることが趣味．とはいっても，自分が写真に映ることはあんまり好きやないね．</p>
+            <div className={styles.spacer}></div>
+            <Link href='/hobby/photo'>See more...:.</Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
