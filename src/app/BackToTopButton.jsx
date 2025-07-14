d5eaca81fb568d5e6be2519a5ec426e4def171f6
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-14 23:10:50 +0900

    BackToTopButton.jsx を更新

--- src/app/BackToTopButton.jsx
+++ src/app/BackToTopButton.jsx
@@ -42,7 +42,7 @@ export default function BackToTopButton() {
         padding: '10px 10px',
       }}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill='currentColor'><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z"/>
     </button>
   );
 }
