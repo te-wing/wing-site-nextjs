bd0af04b6b668f6e44e463ac28ece47fe6915055
Tsubasa <wing@me.wing.osaka>
2025-07-14 18:36:41 +0900

    右の写真

--- src/app/hobby/photo/page.jsx
+++ src/app/hobby/photo/page.jsx
@@ -1,9 +1,13 @@
+import styles from './page.module.scss';
+
 export const metadata = {
   title: '写真 - My Hobby | Wingのサイト',
   description: '写真，プログラミング，音楽など，Wnigとかいう人の趣味を紹介してます！',
 };
 
 export default function PhotoHobbyPage() {
+  // スライド画像のパス
+  const slidesToRight = Array.from({ length: 10 }, (_, i) => `/phtslide/toRight/slide${i + 11}.webp`);
   return (
     <>
       <p>申し訳ありません，準備中です...</p>
