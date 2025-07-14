620520e7d2cb352753351b2797f1a82bf8fe26fd
Tsubasa <wing@me.wing.osaka>
2025-07-14 17:41:03 +0900

    HTML問題を修正

--- src/app/hobby/photo/page.jsx
+++ src/app/hobby/photo/page.jsx
@@ -1,12 +1,12 @@
 export const metadata = {
-    title: '写真 - My Hobby | Wingのサイト',
-    description: '写真，プログラミング，音楽など，Wnigとかいう人の趣味を紹介してます！',
+  title: '写真 - My Hobby | Wingのサイト',
+  description: '写真，プログラミング，音楽など，Wnigとかいう人の趣味を紹介してます！',
 };
 
 export default function PhotoHobbyPage() {
-    return (
-        <>
-            <p>申し訳ありません，準備中です...</p>
-        </>
-    );
+  return (
+    <>
+      <p>申し訳ありません，準備中です...</p>
+    </>
+  );
 }
