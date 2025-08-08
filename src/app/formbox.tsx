42881bf82f9cf4b0f6c0bb9fc11a3f62e77eeef8
wing-0902 <tsubasa@me.wing.osaka>
2025-08-08 13:25:52 +0900

    formbox

--- src/app/formbox.tsx
+++ src/app/formbox.tsx
@@ -3,7 +3,7 @@ import styles from './formbox.module.scss';
 export default function FormBox() {
   return(
     <div className={styles.formBox}>
-      <iframe className={styles.formIframe} src='https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__SvF4cxUQVRaVkQ2VjlTN0NSMlRCNUY5U0o0WEpFMi4u&rcdb8ad6525a34d37b55f1212ddb877b9=%22wing.osaka%22'></iframe>
+      
     </div>
   )
 }
